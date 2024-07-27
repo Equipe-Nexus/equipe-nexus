88417c1867724a7174a30e9399b7cd21a4fc86e8
yVictorK <victorkossmann22@gmail.com>
2024-07-27 00:29:53 -0400

    amanha arrumo o que falta

---
+++ src/screens/LandingPage/Components/Section3/index.tsx
@@ -0,0 +1,20 @@
+import './styles.css';
+
+export function Section3() {
+    return (
+        <main>
+            <div className="section-3" >
+                <div className="section3__title">
+                    <h1 className="title" >Com a nossa equipe você pode impulsonar a sua empresa!</h1>
+                </div>
+                <div className='section-3__subtiltes__and__images' >
+                    <p className='section-3__subtitle' >Temos uma equipe de desenvolvedores de todas as areas</p>
+                    <div className='section-3__images__section' >
+                        <div className='section-3__images__section__1'></div>
+                        <div className='section-3__images__section__2'></div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
--- src/screens/LandingPage/index.tsx
+++ src/screens/LandingPage/index.tsx
@@ -1,18 +1,9 @@
-import './styles.css';
+import { Section3 } from './Components/Section3';
 
 export function LandingPage() {
     return (
         <main>
-            <div className="section-3" >
-                <h1 className="section-3__title" >Com a nossa equipe você pode impulsonar a sua empresa!</h1>
-                <div className='section-3__subtiltes__and__images' >
-                    <p className='section-3__subtitle' >Temos uma equipe de desenvolvedores de todas as areas</p>
-                    <div className='section-3__images__section' >
-                        <div className='section-3__images__section__1'></div>
-                        <div className='section-3__images__section__2'></div>
-                    </div>
-                </div>
-            </div>
+            <Section3 />
         </main>
     )
 }
